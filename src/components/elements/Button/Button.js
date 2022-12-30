24944831b306f0347abee920bb56af6d1beb2f07
Lendra <lendradx@gmail.com>
2022-12-30 10:31:02 +0700

    Updating Button.js Adding icon support changing from outlined to variant="outline-variantName" instead

--- src/components/elements/Button/Button.js
+++ src/components/elements/Button/Button.js
@@ -1,29 +1,41 @@
 import React from "react";
 
-function setVariant(variantName, outlined) {
-	// Default color or primary color
-	let bg = `bg-blue-500 hover:bg-blue-600 text-white ${outlined ? "outline outline-offset-4 outline-blue-500 hover:outline-blue-600" : ""}`;
+/**
+ *
+ * @param {string} variantName
+ * @returns
+ */
+function setVariant(variantName) {
+	let bg;
 	let text = "text-white";
-	console.log(variantName);
-
 	// Determine
 	switch (variantName) {
+		case "outline-danger":
 		case "danger":
-			bg = `bg-red-500 hover:bg-red-600 ${outlined ? "outline outline-offset-4 outline-red-500 hover:outline-red-600" : ""}`;
+			bg = `bg-red-500 hover:bg-red-600 ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-red-500 hover:outline-red-600" : ""}`;
 			break;
+		case "outline-warning":
 		case "warning":
-			bg = `bg-orange-500 hover:bg-orange-600 ${outlined ? "outline outline-offset-4 outline-orange-500" : ""}`;
+			bg = `bg-orange-500 hover:bg-orange-600 ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-orange-500" : ""}`;
 			break;
+		case "outline-info":
 		case "info":
-			bg = `bg-cyan-500 hover:bg-cyan-600 ${outlined ? "outline outline-offset-4 outline-cyan-500 hover:outline-cyan-600" : ""}`;
+			bg = `bg-cyan-500 hover:bg-cyan-600 ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-cyan-500 hover:outline-cyan-600" : ""}`;
 			break;
+		case "outline-success":
 		case "success":
-			bg = `bg-green-500 hover:bg-green-600 ${outlined ? "outline outline-offset-4 outline-green-500 hover:outline-green-600" : ""}`;
+			bg = `bg-green-500 hover:bg-green-600 ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-green-500 hover:outline-green-600" : ""}`;
 			break;
+		case "outline-indigo":
 		case "indigo":
-			bg = `bg-indigo-500 hover:bg-indigo-600 ${outlined ? "outline outline-offset-4 outline-indigo-500 hover:outline-indigo-600" : ""}`;
+			bg = `bg-indigo-500 hover:bg-indigo-600 ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-indigo-500 hover:outline-indigo-600" : ""}`;
 			break;
+		default:
+			// Default color or primary color
+			bg = `bg-blue-500 hover:bg-blue-600 text-white ${variantName.startsWith("outline-") ? "outline outline-offset-4 outline-blue-500 hover:outline-blue-600" : ""}`;
+			text = "text-white";
 	}
+
 	return `${bg} ${text}`;
 }
 
@@ -37,9 +49,14 @@ function setSize(size) {
 	// Pick default padding if size doesn't specified
 	if (!size) return defaultSize;
 	// Determine padding based on size
-	return size == "sm" ? defaultSize : size == "md" ? "px-4 py-2" : size == "lg" ? "px-6 py-3" : size == "xl" ? "px-8 py-4" : defaultSize;
+	return size === "sm" ? defaultSize : size === "md" ? "px-4 py-2" : size === "lg" ? "px-6 py-3" : size === "xl" ? "px-8 py-4" : defaultSize;
 }
 
+/**
+ *
+ * @param {string} rounded
+ * @returns
+ */
 function setRounded(rounded) {
 	if (!rounded) return "rounded";
 	return rounded.startsWith("rounded-") ? rounded : `rounded-${rounded}`;
@@ -50,17 +67,24 @@ function setRounded(rounded) {
  * properties:
  * @label Button label
  * @onClick Call function / method if button clicked
- * @variant Button variant (danger, indigo, info, success, warning)
+ * @variant Button variant (danger, indigo, outline-danger, outline-indigo, etc)
  * @size Button size (sm, md, lg, xl)
- * @outlined Button outlined (optional)
  * @rounded Button rounded
- * @className Your custom class
+ * @icon Button icon [optional]
+ * @iconAlignment Alignment of icon (left, right) [optional]
+ * @className Your custom class gap-2
  */
 const Button = React.memo(function Button(data) {
-	let classList = `${setSize(data.size)} ${setVariant(data.variant, data.outlined)} ${setRounded(data.rounded)} ${data.className ? data.className : ""}`;
+	// Formatted Classes
+	let classList = `text-base flex items-center gap-2 ${data.iconAlignment === "right" ? "flex-row-reverse" : "flex-row"} ${setSize(data.size)} ${setVariant(
+		data.variant,
+		data.outlined
+	)} ${setRounded(data.rounded)} ${data.className ? data.className : ""}
+	`;
+
 	return (
-		<button className={classList}  onClick={data.onClick}>
-			{data.label}
+		<button className={classList.trim()} onClick={data.onClick}>
+			{data.icon} {data.label}
 		</button>
 	);
 });
--- src/components/elements/Button/Preview.js
+++ src/components/elements/Button/Preview.js
@@ -1,41 +1,73 @@
-import React from 'react'
-import Button from '../components/elements/Button/Button'
+import React from "react";
+import Button from "../components/elements/Button/Button";
 
 function Preview() {
-    return (
-        <div className="container px-5 py-5 flex flex-col gap-5">
-            <div className="flex items-center gap-5 mb-3">
-                <Button type="button" size="sm" variant="danger" outlined rounded="lg" label="SM Button" />
-                <Button type="button" size="md" variant="danger" outlined rounded="lg" label="MD Button" />
-                <Button type="button" size="lg" variant="danger" outlined rounded="lg" label="LG Button" />
-                <Button type="button" size="xl" variant="danger" outlined rounded="lg" label="XL Button" />
-            </div>
-            <div className="flex items-center gap-5 mb-3">
-                <Button type="button" size="sm" variant="success" outlined rounded="lg" label="SM Button" />
-                <Button type="button" size="md" variant="success" outlined rounded="lg" label="MD Button" />
-                <Button type="button" size="lg" variant="success" outlined rounded="lg" label="LG Button" />
-                <Button type="button" size="xl" variant="success" outlined rounded="lg" label="XL Button" />
-            </div>
-            <div className="flex items-center gap-5 mb-3">
-                <Button type="button" size="sm" outlined variant="warning" rounded="lg" label="SM Button" />
-                <Button type="button" size="md" outlined variant="warning" rounded="lg" label="MD Button" />
-                <Button type="button" size="lg" outlined variant="warning" rounded="lg" label="LG Button" />
-                <Button type="button" size="xl" outlined variant="warning" rounded="lg" label="XL Button" />
-            </div>
-            <div className="flex items-center gap-5 mb-3">
-                <Button type="button" size="sm" variant="info" outlined rounded="lg" label="SM Button" />
-                <Button type="button" size="md" variant="info" outlined rounded="lg" label="MD Button" />
-                <Button type="button" size="lg" variant="info" outlined rounded="lg" label="LG Button" />
-                <Button type="button" size="xl" variant="info" outlined rounded="lg" label="XL Button" />
-            </div>
-            <div className="flex items-center gap-5 mb-3">
-                <Button type="button" size="sm" variant="indigo" outlined rounded="lg" label="SM Button" />
-                <Button type="button" size="md" variant="indigo" outlined rounded="lg" label="MD Button" />
-                <Button type="button" size="lg" variant="indigo" outlined rounded="lg" label="LG Button" />
-                <Button type="button" size="xl" variant="indigo" outlined rounded="lg" label="XL Button" />
-            </div>
-        </div>
-    )
+	return (
+		<div className="container px-5 py-5 flex flex-col gap-5">
+			<h1>Non Outlined</h1>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="danger" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="danger" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="danger" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="danger" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="success" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="success" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="success" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="success" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="warning" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="warning" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="warning" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="warning" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="info" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="info" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="info" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="info" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="indigo" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="indigo" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="indigo" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="indigo" rounded="lg" label="XL Button" />
+			</div>
+			<h1>Outlined</h1>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="outline-danger" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="outline-danger" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="outline-danger" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="outline-danger" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="outline-success" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="outline-success" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="outline-success" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="outline-success" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="outline-warning" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="outline-warning" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="outline-warning" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="outline-warning" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="outline-info" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="outline-info" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="outline-info" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="outline-info" rounded="lg" label="XL Button" />
+			</div>
+			<div className="flex items-center gap-5 mb-3">
+				<Button type="button" size="sm" variant="outline-indigo" rounded="lg" label="SM Button" />
+				<Button type="button" size="md" variant="outline-indigo" rounded="lg" label="MD Button" />
+				<Button type="button" size="lg" variant="outline-indigo" rounded="lg" label="LG Button" />
+				<Button type="button" size="xl" variant="outline-indigo" rounded="lg" label="XL Button" />
+			</div>
+		</div>
+	);
 }
 
 export default Preview;
