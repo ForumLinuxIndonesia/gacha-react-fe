705dac71af71f47c51530624e2c72e17f33df8d6
daviddev-creator <santosodavid89@gmail.com>
2022-12-30 21:29:26 +0700

    homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../components/Banner";
 import Header from "../components/pages/Header";
+import RecommendedFor from "../components/RecommendedFor";
 
 function Home() {
 	return (
 		<div className="main">
 			<Header />
 			<Banner />
+			<RecommendedFor />
 		</div>
 	);
 }
